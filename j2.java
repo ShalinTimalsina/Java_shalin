d95d96b5c1a5a89166300f3974319827f5e78a46
ShalinTimalsina <shalintimalsina12@gmail.com>
2025-11-18 17:47:23 +0545

    This is the first java commit

---
+++ j2.java
@@ -0,0 +1,8 @@
+public class j2 {
+    public static void Main(String[] param) {
+        System.out.println("Hi this is shalin");
+        System.out.println(param[0]);
+        System.out.println(param[1]);
+
+    }
+}
---
+++ java_class3.java
@@ -0,0 +1,5 @@
+public class java_class3 {
+public static void main(String[]args){
+    System.out.println("Hi this is java ");
+}
+}
