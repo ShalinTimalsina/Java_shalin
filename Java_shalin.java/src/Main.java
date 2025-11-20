ca1be8af17e8d0dead599c0591f0c72d803aedc1
ShalinTimalsina <shalintimalsina12@gmail.com>
2025-11-20 08:26:18 +0545

    Basic Arithmetic Operations

---
+++ Java_shalin.java/src/Main.java
@@ -0,0 +1,18 @@
+void main() {
+    int number1 = 8;
+    int number2 = 5;
+    int additionResult = number1 + number2;
+    int subtractionResult = number1 - number2;
+    int multiplicationResult = number1 * number2;
+    int divisionResult = number1 / number2;
+    int moduloResult = number1 % number2;
+
+    System.out.println("Addition result is:" + additionResult);
+    IO.println("Subtraction result is:" + subtractionResult);
+    IO.println("Multiplication result is:" + multiplicationResult);
+    IO.println("Division result is:" + divisionResult);
+    IO.println("Modulo result is:" + moduloResult);
+    IO.println("Hello world");
+
+}
+
